1e12fab17bfca3a4c1339cedda865ccdfbdf20a0
Konschier <maykonschier@gmail.com>
2020-11-04 15:02:16 -0300

    Commmit de exercicios de treino

---
+++ desafio2.cs
@@ -0,0 +1,14 @@
+string name = "Bob";
+int mensage = 3;
+decimal temperature = 34.4m;
+
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(mensage);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius");
+
+
---
+++ desafio4.cs
@@ -0,0 +1,3 @@
+int fahrenheit = 94;
+decimal multi = 5m / 9m;
+Console.WriteLine("The temperature is " + (fahrenheit - 32) * multi);
